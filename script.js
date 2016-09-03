8b8108d5cf6f18fdc11ac2e08ac64118de6c4e5f
Sahar Jafari <shr.jafari@gmail.com>
2016-09-02 17:11:57 -0700

    remove duplicate code

--- script.js
+++ script.js
@@ -10,13 +10,5 @@ $(document).ready(function(){
     easing: function(x, t, b, c, d) { return b+(t/d)*c ; }
 	});
 	},1000);
-
-	// $("#hand").rotate({
-	// 	angle: 0,
- //    center: ["50%", "100%"],
- //    animateTo: 360,
- //    duration: 60000,
- //    easing: function(x, t, b, c, d) { return b+(t/d)*c ; }
-	// })
 	
 });
