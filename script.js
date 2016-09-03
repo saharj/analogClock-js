c0b92790b251cbaa8bb9250f98aaf47ed1b551e9
Sahar Jafari <shr.jafari@gmail.com>
2016-09-02 17:10:20 -0700

    make the hand stop at each second

--- script.js
+++ script.js
@@ -1,11 +1,22 @@
 $(document).ready(function(){
 
-	$("#hand").rotate({
-		angle: 0,
+	var angle = 0;
+
+	setInterval(function(){
+	  angle+=6;
+	  $("#hand").rotate({
+		angle: angle,
     center: ["50%", "100%"],
-    animateTo: 360,
-    duration: 60000,
     easing: function(x, t, b, c, d) { return b+(t/d)*c ; }
-	})
+	});
+	},1000);
+
+	// $("#hand").rotate({
+	// 	angle: 0,
+ //    center: ["50%", "100%"],
+ //    animateTo: 360,
+ //    duration: 60000,
+ //    easing: function(x, t, b, c, d) { return b+(t/d)*c ; }
+	// })
 	
 });
